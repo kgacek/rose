688cdf981bb8a8c28635bf24dd9f893f0fe5c2db
Krzysztof Gacek <krzysztof.gacek@nokia.com>
2019-02-20 22:28:08 +0100

    new admins

--- static/js/intentions.js
+++ static/js/intentions.js
@@ -83,7 +83,7 @@ function LoginCallback(response) {
         fb_user_psid = false; // todo: poprawic
         fb_user_id = response.authResponse["userID"];
         showStatus(fb_user_id);
-        if (['2648811858479034', '2364148863618959', '2417174628322246', '2816839405023046', '322686561691681'].indexOf(response.authResponse["userID"]) >= 0) { //TODO: trzeba dodac liste adminow
+        if (['2648811858479034', '2364148863618959', '2417174628322246', '2816839405023046', '322686561691681', '838937949798703','1948127701931349','1725926644219720'].indexOf(response.authResponse["userID"]) >= 0) { //TODO: trzeba dodac liste adminow
             updateNavbar('admin');
         }else {
             updateNavbar('connected');
