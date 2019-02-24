19b4d45fa94e53ce7b295852422ddad658de6115
Krzysztof Gacek <krzysiek.gacus@wp.pl>
2019-02-24 10:35:48 +0000

    new admin

--- static/js/admin.js
+++ static/js/admin.js
@@ -48,7 +48,7 @@ function LoginCallback(response) {
     console.log('LoginCallback');
     if (response.status === 'connected') {
         fb_user_id = response.authResponse["userID"];
-        if (['2648811858479034', '2364148863618959', '2417174628322246', '2816839405023046', '322686561691681','838937949798703','1948127701931349','1725926644219720'].indexOf(response.authResponse["userID"]) >= 0) { //TODO: trzeba dodac liste adminow
+        if (['10218775416925342','2648811858479034', '2364148863618959', '2417174628322246', '2816839405023046', '322686561691681','838937949798703','1948127701931349','1725926644219720'].indexOf(response.authResponse["userID"]) >= 0) { //TODO: trzeba dodac liste adminow
             updateNavbar('admin');
             $.getJSON("https://kgacek.pythonanywhere.com/_new_users",
                 function (data) {
