08695de058605b6352732c5828ab835aea24893e
Krzysztof Gacek <krzysztof.gacek@nokia.com>
2019-02-26 22:23:08 +0100

    new admin

--- static/js/index.js
+++ static/js/index.js
@@ -13,7 +13,7 @@ function LoginCallback(response) {
     console.log('indexLoginCallback');
     if (response.status === 'connected') {
         updateNavbar('connected');
-        if (['10218775416925342', '2648811858479034', '2364148863618959', '2417174628322246', '2816839405023046', '322686561691681', '838937949798703','1948127701931349','1725926644219720'].indexOf(response.authResponse["userID"]) >= 0) { //TODO: trzeba dodac liste adminow
+        if (['10205894962648737','10218775416925342', '2648811858479034', '2364148863618959', '2417174628322246', '2816839405023046', '322686561691681', '838937949798703','1948127701931349','1725926644219720'].indexOf(response.authResponse["userID"]) >= 0) { //TODO: trzeba dodac liste adminow
             updateNavbar('admin');
         }
         addNewUser(response.authResponse["userID"])
