7c419542bd57fefc936e91619d2071a7f14bcdd8
Krzysztof Gacek <krzysztof.gacek@nokia.com>
2019-02-26 14:16:37 +0100

    fix

--- static/js/fbload.js
+++ static/js/fbload.js
@@ -1,11 +1,9 @@
 window.fbAsyncInit = function () {
         FB.init({
             appId: '1061023747434571',
-            cookie: true,  // enable cookies to allow the server to access
-                           // the session
             xfbml: true,  // parse social plugins on this page
-            version: 'v3.2', // use graph api version 2.8
-            status : true
+            version: 'v3.2' // use graph api version 2.8
+
         });
         FB.getLoginStatus(function (response) {
             LoginCallback(response);
@@ -18,6 +16,6 @@ window.fbAsyncInit = function () {
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        js.src = "https://connect.facebook.net/pl_PL/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
